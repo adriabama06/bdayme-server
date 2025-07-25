d6ea2c568614beed7cd903ca66fe027bfc764ced
adriabama06 <adriabama06@gmail.com>
2025-07-25 18:45:27 +0200

    Fix user update logic: correct variable references and add token deletion for updated users

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,9 @@
+import crypto from "crypto";
+
 import { Router } from "express";
 import { get_user, update_user } from "../controller/user.js";
 import middleware_auth from "../middlewares/auth.js";
+import redis_client from "../redis.js";
 
 const app = Router();
 
@@ -15,7 +18,7 @@ app.get("/", middleware_auth, async (req, res) => {
 app.post("/update", middleware_auth, async (req, res) => {
     const data = req.body;
 
-    if (typeof data.email !== "string" || typeof data.password !== "string" || email.length > 255 || password.length > 255) {
+    if (typeof data.email !== "string" || typeof data.password !== "string" || data.email.length > 255 || data.password.length > 255) {
         return res.status(400).json({
             error: "Email & Password must be a string"
         });
@@ -42,6 +45,36 @@ app.post("/update", middleware_auth, async (req, res) => {
             });
         }
     }
+
+    const userId = parseInt(req.user.id);
+    
+    let keysToDelete = [];
+
+    // Delete all tokens related to that user
+    let cursor = 0;
+    do {
+        const reply = await redis_client.scan(cursor, {
+            MATCH: 'tokens:*',
+            COUNT: 100
+        });
+
+        cursor = parseInt(reply.cursor);
+        const keys = reply.keys;
+
+        for (const key of keys) {
+            const value = await redis_client.get(key);
+
+            try {
+                if (parseInt(value) === userId) {
+                    keysToDelete.push(key);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    } while (cursor !== 0);
+
+    await redis_client.del(keysToDelete);
 
     const newUser = await get_user(req.user.id);
 

d0f5119cb6e83c481036ae79cc0938e023848917
Adria <adriabama06@gmail.com>
2025-08-10 12:44:06 +0000

    Limit client version

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ const app = express();
 app.use(express.json());
 app.set('trust proxy', process.env.TRUST_PROXY?.split(",") ?? []); // https://expressjs.com/en/guide/behind-proxies.html
 
+// Check client version
+app.use((req, res, next) => {
+    const client_version = req.headers["client-version"];
+
+    if(client_version === undefined) {
+        return res.status(400).json({
+            error: "Your client is outdated, your version might be lower than 0.9.0, so, is not compatible with the current version of the API"
+        });
+    }
+
+    if(client_version === "custom") {
+        return next();
+    }
+
+    const [top, med, low] = client_version.split(".");
+
+    // 0.9.0
+    if(top <= 0 && med <= 9 && low <= 0) {
+        return res.status(400).json({
+            error: "The client version 0.9.0 or lower is not compatible with this API version"
+        });
+    }
+
+    next();
+});
+
 import user_api from "./api/user.js";
 import auth_api from "./api/auth.js";
 import profile_api from "./api/profile.js";

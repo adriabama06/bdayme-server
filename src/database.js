4644c8ac228d8f7fdb41f9cbc835dc77ac6f8fbb
Adria <adriabama06@gmail.com>
2025-08-10 11:20:08 +0000

    Auto update database with the new table

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const MAX_ABOUTME_LENGTH = 1024;
+
 import pg from "pg";
 
 const pg_client = new pg.Client({
@@ -13,8 +15,37 @@ const pg_client = new pg.Client({
 
 pg_client.connect();
 
-pg_client.on("connect", () => {
+pg_client.on("connect", async () => {
     console.log("[PG] Connected to Postgres");
+    console.log("[PG] Checking if the database is updated for this version.");
+
+    // add-aboutme
+    try {
+        const res = await pg_client.query(`
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = 'profiles' AND column_name = 'aboutme';
+        `);
+
+        if (res.rows.length === 0) {
+            console.log("[DB] 'aboutme' does not exists in 'profiles', it will be created.");
+
+            await pg_client.query(`
+                ALTER TABLE profiles
+                ADD COLUMN aboutme VARCHAR(${MAX_ABOUTME_LENGTH}) NOT NULL DEFAULT '';
+            `);
+
+            console.log("[DB] 'aboutme' has been created");
+        }
+    } catch (err) {
+        console.log("[PG] Error checking or creating if the 'aboutme' exists in 'profiles'.");
+        console.error(err);
+
+        // Stop the code because can't know if database is ok.
+        process.exit(1);
+    }
+
+    console.log("[PG] All ok.");
 });
 
 pg_client.on("error", (err) => {
